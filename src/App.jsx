2fe18038ed1a2b5029671719a8e6411575f9c098
Yashraj_K <yashrajkamble7727@gamil.com>
2024-07-27 18:57:33 +0530

    modify UI

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,9 @@ function App() {
     <>
       <div>
         <div className="app">
+
           <div className="sideBar">
+
             <div className="upperSide">
               <div className="upperSideTop">
                 <img src={gptLogo} alt="Logo" className="logo" />
@@ -62,9 +64,25 @@ function App() {
                 <p className="txt">
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
                   Voluptatem cum, natus quasi eius excepturi repellendus!
-                  Laborum, possimus velit corrupti est natus, eligendi vel quam Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum, iusto explicabo reiciendis nemo eum incidunt maiores cupiditate delectus eligendi provident iure eos et hic aliquid, ut ipsum architecto voluptatem recusandae, repellendus aperiam! Molestias, aliquid. Odit, iste doloremque numquam ipsa cupiditate amet possimus, nostrum, voluptate cum velit accusantium beatae? Veritatis ratione harum iure perferendis aliquid hic. Magnam aperiam doloremque veritatis voluptatibus odit vel molestiae iusto odio sapiente id sequi dignissimos porro atque nisi perferendis, vero natus voluptatem tenetur laudantium quas rerum veniam assumenda? Cumque, saepe quae autem molestias aliquid omnis, sint eum repellendus accusamus incidunt repellat dolore! Totam sequi quae, vel, hic at voluptatibus inventore dicta voluptate sit quibusdam, omnis itaque fuga?
+                  Laborum, possimus velit corrupti est natus, eligendi vel quam
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Earum, iusto explicabo reiciendis nemo eum incidunt maiores
+                  cupiditate delectus eligendi provident iure eos et hic
+                  aliquid, ut ipsum architecto voluptatem recusandae,
+                  repellendus aperiam! Molestias, aliquid. Odit, iste doloremque
+                  numquam ipsa cupiditate amet possimus, nostrum, voluptate cum
+                  velit accusantium beatae? Veritatis ratione harum iure
+                  perferendis aliquid hic. Magnam aperiam doloremque veritatis
+                  voluptatibus odit vel molestiae iusto odio sapiente id sequi
+                  dignissimos porro atque nisi perferendis, vero natus
+                  voluptatem tenetur laudantium quas rerum veniam assumenda?
+                  Cumque, saepe quae autem molestias aliquid omnis, sint eum
+                  repellendus accusamus incidunt repellat dolore! Totam sequi
+                  quae, vel, hic at voluptatibus inventore dicta voluptate sit
+                  quibusdam, omnis itaque fuga?
                 </p>
               </div>
+              
               <div className="chat bot">
                 <img className="chatImg" src={gptImgLogo} alt="" />
                 <p className="txt">
@@ -77,7 +95,22 @@ function App() {
                   obcaecati sed commodi praesentium autem ut incidunt! Aut,
                   tenetur sunt hic omnis quos quis tempora molestias vero
                   pariatur. Voluptatum esse possimus reiciendis sint aliquam ex
-                  doloribus dignissimos! Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio nemo ipsum, ad quibusdam qui cum nulla excepturi tempora sit nesciunt eius assumenda molestiae, officia corrupti a quis in ratione? Sint non possimus, placeat nisi nesciunt consequuntur velit neque a! Dolorem adipisci nulla porro ipsa dicta autem exercitationem sequi reprehenderit. Soluta omnis explicabo perferendis blanditiis. Necessitatibus repudiandae vero exercitationem accusamus reiciendis qui inventore, magni rerum possimus, magnam illo eligendi cum, deserunt odio nulla. Quasi ad optio assumenda earum, magnam laudantium eos exercitationem iure, harum dolorem fuga, alias eaque! Vitae, tempora distinctio officiis quos nobis exercitationem voluptates debitis sequi, nesciunt, quam ratione. Culpa, deserunt? Consequatur praesentium voluptatibus dolore facere necessitatibus obcaecati nisi omnis?
+                  doloribus dignissimos! Lorem ipsum dolor sit amet consectetur
+                  adipisicing elit. Optio nemo ipsum, ad quibusdam qui cum nulla
+                  excepturi tempora sit nesciunt eius assumenda molestiae,
+                  officia corrupti a quis in ratione? Sint non possimus, placeat
+                  nisi nesciunt consequuntur velit neque a! Dolorem adipisci
+                  nulla porro ipsa dicta autem exercitationem sequi
+                  reprehenderit. Soluta omnis explicabo perferendis blanditiis.
+                  Necessitatibus repudiandae vero exercitationem accusamus
+                  reiciendis qui inventore, magni rerum possimus, magnam illo
+                  eligendi cum, deserunt odio nulla. Quasi ad optio assumenda
+                  earum, magnam laudantium eos exercitationem iure, harum
+                  dolorem fuga, alias eaque! Vitae, tempora distinctio officiis
+                  quos nobis exercitationem voluptates debitis sequi, nesciunt,
+                  quam ratione. Culpa, deserunt? Consequatur praesentium
+                  voluptatibus dolore facere necessitatibus obcaecati nisi
+                  omnis?
                 </p>
               </div>
             </div>
@@ -91,6 +124,7 @@ function App() {
               </div>
               <p>ChatGPT can make mistakes. Check important info.</p>
             </div>
+
           </div>
         </div>
       </div>
