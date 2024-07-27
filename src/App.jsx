22bb123495d4483442ac03d50b67baefbeb6ed61
Yashraj_K <yashrajkamble7727@gamil.com>
2024-07-27 14:01:26 +0530

    upload all icons in side bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import "./App.css";
 import gptLogo from "./assets/chatgpt.svg";
+import addBtn from "./assets/add-30.png";
+import msgIcon from "./assets/message.svg";
+import home from "./assets/home.svg";
+import saved from "./assets/bookmark.svg";
+import rocket from "./assets/rocket.svg";
 
 function App() {
   return (
@@ -13,25 +18,40 @@ function App() {
                 <span className="brand">ChatGPT</span>
                 <div>
                   <button className="midBtn">
-                    <img src="" alt="" className="addBtn" />
+                    <img src={addBtn} alt="New Chat" className="addBtn" />
                     New Chat
                   </button>
                 </div>
-                
+
                 <div className="upperSideBottom">
                   <button className="query">
-                    <img src="" alt="" />
+                    <img src={msgIcon} alt="Query" />
                     What is React JS?
                   </button>
                   <button className="query">
-                    <img src="" alt="" />
+                    <img src={msgIcon} alt="Query" />
                     What is Next JS?
                   </button>
                 </div>
               </div>
             </div>
 
-            <div className="lowerSide"></div>
+            <div className="lowerSide">
+              <div className="listItems">
+                <img src={home} alt="Home" className="listItemsImg" />
+                Home
+              </div>
+
+              <div className="listItems">
+                <img src={saved} alt="Saved" className="listItemsImg" />
+                Saved
+              </div>
+
+              <div className="listItems">
+                <img src={rocket} alt="Upgrade" className="listItemsImg" />
+                Upgrade
+              </div>
+            </div>
           </div>
 
           <div className="main"></div>
