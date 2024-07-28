ef06260406a337e3bf76381640b6375e3fc8b713
Yashraj_K <yashrajkamble7727@gamil.com>
2024-07-28 17:17:01 +0530

    all work done

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,49 @@ import sendBtn from "./assets/send.svg";
 import userIcon from "./assets/user-icon.png";
 import gptImgLogo from "./assets/chatgptLogo.svg";
 import { sendMsgToOpenAI } from "./openai";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+  const msgEnd = useRef();
   const [input, setInput] = useState("");
+  const [message, setMessage] = useState([
+    {
+      text: "hey chatgpt, how are you?",
+      isBot: true,
+    },
+  ]);
+
+  useEffect(() => {
+    msgEnd.current.scrollIntoView();
+  }, [message]);
 
   const handleSend = async () => {
+    const text = input;
+    setInput("");
+    setMessage([...message, { text, isBot: false }]);
     const res = await sendMsgToOpenAI(input);
-    console.log(res);
+    setMessage([
+      ...message,
+      { text, isBot: false },
+      { text: res, isBot: true },
+    ]);
   };
+  const handleEnter = async (e) => {
+    if (e.key === "Enter") await handleSend();
+  };
+
+  const handleQuery = async (e) => {
+    const text = e.target.value;
+    setInput("");
+    setMessage([...message, { text, isBot: false }]);
+    const res = await sendMsgToOpenAI(input);
+    setMessage([
+      ...message,
+      { text, isBot: false },
+      { text: res, isBot: true },
+    ]);
+  };
+
   return (
     <>
       <div>
@@ -28,17 +62,30 @@ function App() {
                 <img src={gptLogo} alt="Logo" className="logo" />
                 <span className="brand">ChatGPT</span>
               </div>
-              <button className="midBtn">
+              <button
+                className="midBtn"
+                onClick={() => {
+                  window.location.reload();
+                }}
+              >
                 <img src={addBtn} alt="New Chat" className="addBtn" />
                 New Chat
               </button>
 
               <div className="upperSideBottom">
-                <button className="query">
+                <button
+                  className="query"
+                  onClick={handleQuery}
+                  value={"What is React JS?"}
+                >
                   <img src={msgIcon} alt="Query" />
                   What is React JS?
                 </button>
-                <button className="query">
+                <button
+                  className="query"
+                  onClick={handleQuery}
+                  value={"What is Next JS?"}
+                >
                   <img src={msgIcon} alt="Query" />
                   What is Next JS?
                 </button>
@@ -65,60 +112,17 @@ function App() {
 
           <div className="main">
             <div className="chats">
-              <div className="chat">
-                <img className="chatImg" src={userIcon} alt="" />
-                <p className="txt">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Voluptatem cum, natus quasi eius excepturi repellendus!
-                  Laborum, possimus velit corrupti est natus, eligendi vel quam
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Earum, iusto explicabo reiciendis nemo eum incidunt maiores
-                  cupiditate delectus eligendi provident iure eos et hic
-                  aliquid, ut ipsum architecto voluptatem recusandae,
-                  repellendus aperiam! Molestias, aliquid. Odit, iste doloremque
-                  numquam ipsa cupiditate amet possimus, nostrum, voluptate cum
-                  velit accusantium beatae? Veritatis ratione harum iure
-                  perferendis aliquid hic. Magnam aperiam doloremque veritatis
-                  voluptatibus odit vel molestiae iusto odio sapiente id sequi
-                  dignissimos porro atque nisi perferendis, vero natus
-                  voluptatem tenetur laudantium quas rerum veniam assumenda?
-                  Cumque, saepe quae autem molestias aliquid omnis, sint eum
-                  repellendus accusamus incidunt repellat dolore! Totam sequi
-                  quae, vel, hic at voluptatibus inventore dicta voluptate sit
-                  quibusdam, omnis itaque fuga?
-                </p>
-              </div>
-
-              <div className="chat bot">
-                <img className="chatImg" src={gptImgLogo} alt="" />
-                <p className="txt">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Voluptatem cum, natus quasi eius excepturi repellendus!
-                  Laborum, possimus velit corrupti est natus, eligendi vel quam
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                  Quibusdam ex aliquid, in adipisci laborum voluptatum a cum
-                  dolorum ipsam illum reiciendis atque similique optio magnam,
-                  obcaecati sed commodi praesentium autem ut incidunt! Aut,
-                  tenetur sunt hic omnis quos quis tempora molestias vero
-                  pariatur. Voluptatum esse possimus reiciendis sint aliquam ex
-                  doloribus dignissimos! Lorem ipsum dolor sit amet consectetur
-                  adipisicing elit. Optio nemo ipsum, ad quibusdam qui cum nulla
-                  excepturi tempora sit nesciunt eius assumenda molestiae,
-                  officia corrupti a quis in ratione? Sint non possimus, placeat
-                  nisi nesciunt consequuntur velit neque a! Dolorem adipisci
-                  nulla porro ipsa dicta autem exercitationem sequi
-                  reprehenderit. Soluta omnis explicabo perferendis blanditiis.
-                  Necessitatibus repudiandae vero exercitationem accusamus
-                  reiciendis qui inventore, magni rerum possimus, magnam illo
-                  eligendi cum, deserunt odio nulla. Quasi ad optio assumenda
-                  earum, magnam laudantium eos exercitationem iure, harum
-                  dolorem fuga, alias eaque! Vitae, tempora distinctio officiis
-                  quos nobis exercitationem voluptates debitis sequi, nesciunt,
-                  quam ratione. Culpa, deserunt? Consequatur praesentium
-                  voluptatibus dolore facere necessitatibus obcaecati nisi
-                  omnis?
-                </p>
-              </div>
+              {message.map((message, i) => (
+                <div key={i} className={message.isBot ? "chat bot" : "chat"}>
+                  <img
+                    className="chatImg"
+                    src={message.isBot ? gptImgLogo : userIcon}
+                    alt=""
+                  />
+                  <p className="txt">{message.text}</p>
+                </div>
+              ))}
+              <div ref={msgEnd} />
             </div>
 
             <div className="chatFooter">
@@ -127,6 +131,7 @@ function App() {
                   type="text"
                   placeholder="Message ChatGPT"
                   value={input}
+                  onKeyDown={handleEnter}
                   onChange={(e) => {
                     setInput(e.target.value);
                   }}
